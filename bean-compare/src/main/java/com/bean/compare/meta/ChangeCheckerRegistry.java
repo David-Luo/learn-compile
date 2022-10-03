f541c57c190e40bc95b82dce42e39b7b3765c049
David-Luo <weixingluo@gmail.com>
2022-10-03 17:59:31 +0800

    修复继承后取不到范型的bug

--- bean-compare/src/main/java/com/bean/compare/meta/ChangeCheckerRegistry.java
+++ bean-compare/src/main/java/com/bean/compare/meta/ChangeCheckerRegistry.java
@@ -48,19 +48,32 @@ public class ChangeChekerRegistry implements ServiceDefinitionRegistry<ChangeChe
     }
 
     private <S> ChangeCheckerDefinition map(Class<? extends ChangeChecker> clazz) {
-        ParameterizedType type = (ParameterizedType) findType(clazz, ChangeChecker.class);
-        if (type == null) {
-            throw new IllegalArgumentException("ChangeChecker must be parameterized");
-        }
-        Type[] types = type.getActualTypeArguments();
         Class<?> targetClass=null;
-        if(types[0] instanceof Class){
-            targetClass = (Class<?>) types[0];
-        }else if(types[0] instanceof  TypeVariable){
-            TypeVariable<?> typeVariable = (TypeVariable<?>) types[0];
-            targetClass =(Class<?>)typeVariable.getBounds()[0];
-        }else{
-            throw new IllegalArgumentException("ChangeChecker must be parameterized");
+        TypeVariable<? extends Class<?>>[] typeParameters =  clazz.getTypeParameters();
+        if(typeParameters.length>0){
+            TypeVariable<? extends Class<?>> typeParameter = typeParameters[0];
+            Type[] bounds = typeParameter.getBounds();
+            if (bounds.length==0){
+                targetClass = (Class)typeParameter.getGenericDeclaration();
+            }else {
+                targetClass = (Class<?>)bounds[0];
+            }
+        }
+        if(targetClass == null) {
+            ParameterizedType type = (ParameterizedType) findType(clazz, ChangeChecker.class);
+            if (type == null) {
+                throw new IllegalArgumentException("ChangeChecker must be parameterized");
+            }
+            Type[] types = type.getActualTypeArguments();
+
+            if (types[0] instanceof Class) {
+                targetClass = (Class<?>) types[0];
+            } else if (types[0] instanceof TypeVariable) {
+                TypeVariable<?> typeVariable = (TypeVariable<?>) types[0];
+                targetClass = (Class<?>) typeVariable.getBounds()[0];
+            } else {
+                throw new IllegalArgumentException("ChangeChecker must be parameterized");
+            }
         }
         int order = 0;
         return new ChangeCheckerDefinition(
@@ -69,8 +82,11 @@ public class ChangeChekerRegistry implements ServiceDefinitionRegistry<ChangeChe
                 order);
     }
 
+    //FixMe 无法处理多重继承时的类型匹配
     private Type findType(Class<?> clazz, Class<?> interfaceClass) {
+
         Type[] types = clazz.getGenericInterfaces();
+
         for (Type type : types) {
             if (type.getTypeName().startsWith(interfaceClass.getCanonicalName())) {
                 return type;
